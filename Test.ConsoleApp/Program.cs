d39d6a864b428c9ba1e44a7397322c61c0d7bb8d
Starinin Andrey <starinin-andrey@ya.ru>
2026-01-18 12:45:23 +0300

    Update

--- Test.ConsoleApp/Program.cs
+++ Test.ConsoleApp/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using Test.Lib;
 
-var a = 5;
-var b = 10;
-var result = Calc.Add(a, b);
+var num1 = 5;
+var num2 = 10;
+var result = Calc.Add(num1, num2);
 
-Console.WriteLine($"{a} + {b} = {result}");
+Console.WriteLine($"{num1} + {num2} = {result}");
